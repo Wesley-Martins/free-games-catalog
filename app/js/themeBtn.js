d2f629a30072d340da1280991077c351fbefe17e
Wésley P.Martins <weslley2013.pinto@gmail.com>
2023-04-04 20:55:25 -0300

    Header e banner iniciais terminados

---
+++ app/js/themeBtn.js
@@ -0,0 +1,5 @@
+const themeBtn = document.getElementById('theme-btn');
+const themeBtnIcon = themeBtn.firstElementChild;
+themeBtn.addEventListener('click', () => {
+    themeBtnIcon.classList.toggle('slide-right');
+})
