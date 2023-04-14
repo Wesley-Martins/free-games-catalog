871e41c9cbd3bfce9198cdf28c686bc8c4061906
Wésley P.Martins <weslley2013.pinto@gmail.com>
2023-04-14 19:58:51 -0300

    Terminado menu-dropdown dinâmico e botão de light/dark-theme removido.

--- app/js/global/themeBtn.js
+++
@@ -1,6 +0,0 @@
-const themeBtn = document.getElementById('theme-btn');
-const themeBtnIcon = themeBtn.firstElementChild;
-
-themeBtn.addEventListener('click', () => {
-    themeBtnIcon.classList.toggle('slide-right');
-})
--- app/js/home-page/dropdown-menu.js
+++ app/js/home-page/dropdown-menu.js
@@ -1,15 +1,39 @@
-const dropdownBtns = document.querySelectorAll('.options__drop-btn');
-const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+const openMenuBtns = document.querySelectorAll('.options__drop-btn');
+const menus = document.querySelectorAll('.dropdown-menu');
+
+const menuOptions = document.querySelectorAll('.dropdown-menu__btn');
+let lastClickedOption;
+
+function toggleDropdown(menu) {
+    menu.classList.toggle('hidden');
+}
 
 document.addEventListener('click', (event) => {
     const element = event.target;
 
-    for(let i = 0; i < dropdownBtns.length; i++) {
-        if(element != dropdownBtns[i] && element != dropdownBtns[i].firstElementChild) {
-            dropdownMenus[i].classList.add('hidden');
+    menuOptions.forEach((option) => {
+        if(element == option) { lastClickedOption = element; }
+    });
+
+    for(let i = 0; i < openMenuBtns.length; i++) {
+        const btn = openMenuBtns[i];
+        const menu = menus[i];
+
+        switch(element) {
+            case btn:
+            case btn.firstElementChild:
+                toggleDropdown(menu);
+                break
+            
+            case lastClickedOption:
+                if(btn.dataset.type == lastClickedOption.dataset.type) {
+                    btn.innerHTML = lastClickedOption.innerText + '<i class="fa-solid fa-circle-chevron-down fa-lg"></i>';
+                }
+                break
+
+            default:
+                menu.classList.add('hidden');
         }
-        else {
-            dropdownMenus[i].classList.toggle('hidden');
-        }
-    }
+    } 
 })
+
